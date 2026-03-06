1f5d7459a1bade57e96bdb32a4662f13889564ea
eliezermoraesss <eliezer.moraes@outlook.com>
2026-03-05 22:21:59 -0300

    feat: Redução na resoluçaõ da imagem da amazon

--- scraper/app.py
+++ scraper/app.py
@@ -107,10 +107,11 @@ def scrape(data: dict):
                     preco_anterior = old_price_locator.first.inner_text().replace("De:", "").strip()
 
                 if page.locator("#landingImage").count() > 0:
-                    imagem_url = (
+                    raw_img = (
                         page.locator("#landingImage").get_attribute("data-old-hires")
                         or page.locator("#landingImage").get_attribute("src")
                     )
+                    imagem_url = re.sub(r"_SL\d+_", "_SL800_", raw_img)
 
             else:
                 browser.close()
